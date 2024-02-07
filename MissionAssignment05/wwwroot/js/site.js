ea72cd9f5fb5f330396a274e8132a3ff7146997c
jacobbigler <jacob.bigler03@gmail.com>
2024-02-07 08:10:33 -0700

    Finished Part 1 (hopefully the only part)

--- MissionAssignment05/wwwroot/js/site.js
+++ MissionAssignment05/wwwroot/js/site.js
@@ -2,3 +2,46 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Load jQuery from a CDN
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+// Wait for jQuery to be loaded
+script.onload = function () {
+    $(function () {
+        // Function to calculate result
+        function calculateResult(hours) {
+            return "$" + (hours * 229.54);
+        }
+
+        // Event handler for button click
+        $("#btnSubmit").on("click", function () {
+            // Get the number of hours from the input field
+            var hours = parseFloat($("#intHours").val().toString());
+
+            // Validate input
+            if (isNaN(hours) || hours <= 0) {
+                alert("Please enter a positive number for hours.");
+                return;
+            }
+            
+            var result = calculateResult(hours);
+            
+            $("#calculated-result").text(result);
+        });
+
+        // Event handler for input field keypress
+        $("#intHours").on("keypress", function (event) {
+            // Check if Enter key was pressed (key code 13)
+            if (event.which == 13) {
+                // Trigger the button click event
+                $("#btnSubmit").trigger("click");
+                event.preventDefault();
+            }
+        });
+    });
+};
+
